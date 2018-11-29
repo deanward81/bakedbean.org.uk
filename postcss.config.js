9db0740b64769e42befea3365a9f626e8084bc60
Dean Ward <dward@stackoverflow.com>
2018-11-29 18:00:23 +0000

    moar optimization; inline fonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
+const url = require('postcss-url')
 
 module.exports = {
     plugins: [
         purgecss({
             content: ['./**/*.html', './config.toml']
+        }),
+        url({
+            url: 'inline',
+            basePath: ['./assets/css']
         })
     ]
 }
